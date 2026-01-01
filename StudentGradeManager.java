5d40a3ed6b2d1c39c16cf2c0b18a51320a0fbf3f
abitsiflow <xtiangzmen@gmail.com>
2026-01-01 15:41:05 +0800

    Add files via upload

--- StudentGradeManager.java
+++ StudentGradeManager.java
@@ -4,7 +4,7 @@
 
 package mimo.studentgrademanager;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 /**
  *
  * @author ADMIN
@@ -29,38 +29,35 @@ public class StudentGradeManager {
             int choice = scan.nextInt();
             scan.nextLine();
             
-            try{
-                 switch(choice){
+         
+            switch(choice){
                 
-                case 1:
-                    student.info();
-                    break;
-                case 2:
+                case 1 -> student.info();
+                
+                case 2 -> {
                     boolean case2Running = true; // idk what should i name but it works...
                     while(case2Running){
-                    System.out.println("");
-                    System.out.println("1. Add Grade");
-                    System.out.println("2. Show Grades");
-                    System.out.println("3. Exit");
-                    int gradeChoice = scan.nextInt();
-                    scan.nextLine();
-                    
-                    switch(gradeChoice){
-                           case 1 -> addGrades.addGrade();
-                           case 2 ->{
-                               addGrades.showGrades();
-                               addGrades.isPassed();
-                           }
-                           case 3 -> {
-                               System.out.println("Exiting.....");
-                               case2Running = false;
-                           }
+                        System.out.println("");
+                        System.out.println("1. Add Grade");
+                        System.out.println("2. Show Grades");
+                        System.out.println("3. Exit");
+                        int gradeChoice = scan.nextInt();
+                        scan.nextLine();
+                        
+                        switch(gradeChoice){
+                            case 1 -> addGrades.addGrade();
+                            case 2 ->{
+                                addGrades.showGrades();
+                            }
+                            case 3 -> {
+                                System.out.println("Exiting.....");
+                                case2Running = false;
+                            }
+                        }
+                        
                     }
-                   
-                   }
-          
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     boolean case3Running = true;
                     while(case3Running){ 
                         System.out.println("");
@@ -70,34 +67,33 @@ public class StudentGradeManager {
                         System.out.println("4. Exit");
                         System.out.print("Enter Choice(1-4): "); // user has 3 choices to input something
                         int showChoice = scan.nextInt();
-                    
-                    switch(showChoice){
-                        case 1 -> {
-                            student.showName();
+                        
+                        switch(showChoice){
+                            case 1 -> {
+                                student.showName();
+                            }
+                            case 2 ->{
+                                student.showId();
+                            }
+                            case 3 ->{
+                                student.showInfo();
+                            }
+                            case 4 ->{
+                                System.out.println("Exiting.....");
+                                case3Running = false;
+                            }
+                            default -> System.out.println("Invalid Choice, Please Try Again!");
                         }
-                        case 2 ->{
-                            student.showId();
-                        }
-                        case 3 ->{
-                            student.showInfo();
-                        }
-                        case 4 ->{
-                            System.out.println("Exiting.....");
-                            case3Running = false;
-                        }
-                        default -> System.out.println("Invalid Choice, Please Try Again!");
+
                     }
-                   
-                    }
-                    break;
-                case 4:
-                     System.out.println("Thank you for using my small program!");
-                     isRunning = false;
-                    break;
+                }
+                case 4 -> {
+                    System.out.println("Thank you for using my small program!");
+                    isRunning = false;
+                }
             }
-                }catch(Exception e){
-                    System.out.println("Invalid choice, Please Enter Number.");
-            }
+              
+            
      
         }
            
--- addGradeOnSubject.java
+++ addGradeOnSubject.java
@@ -4,7 +4,7 @@
  */
 package mimo.studentgrademanager;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 /**
  *
  * @author ADMIN
@@ -12,52 +12,68 @@ import java.util.ArrayList;
 public class addGradeOnSubject {
     Scanner scan = new Scanner(System.in);
     
-    ArrayList<Double> list_grade = new ArrayList<>(); 
-    ArrayList<String> list_subject = new ArrayList<>();
-    
+    static double [] list_grade = new double[2];
+    String[] list_subject = new String[2];
+   
+    /*
     String subject;
     static double grade;
+    I tried to use this but it makes something redundant because it instead making the variable to put a value it become worse.
+    
+    In my theory i think because the   double [] list_grade = new double[2]; has the value of 2 then the variables are reproducing 2 same values because its like the function of the list_grade but its just a copy.
+    UPDATE: IT STILL MULTIPLY
+    
+    */
     
     // i need something to calculate whether the student is passed or failed and also that records.
     // now i need something to select the student then input the choice per subject
     // scratch that idea i think i need to learn more about OOP. 
     
     void addGrade(){
-        System.out.print("Enter Grade: ");
-        grade = scan.nextDouble();
-        scan.nextLine(); // need this to prevent skipping
-        
-        System.out.print("Enter Subject: ");
-        subject = scan.nextLine().trim();
-        
-        list_grade.add(grade);
-        list_subject.add(subject); //inputting grades per subject
-        
+       
+       for(int i = 0; i < list_subject.length; i++){
+            System.out.print("Enter Subject: ");
+            list_subject[i] = scan.nextLine().trim();
+       
+            System.out.print("Enter Grade: ");
+            list_grade[i] = scan.nextDouble();
+            scan.nextLine(); // need this to prevent skipping
+            
+            System.out.println();
+            
+         
+           
+       }
+       
+         for(int i = 0; i < list_subject.length; i++){
+             System.out.println(list_subject[i] + " == " + list_grade[i]);
+         }
+         
+         isPassed();
     }
     
     void showGrades(){
+       
         for(double grades : list_grade){
-          for(String subjects : list_subject){
-            System.out.println(grades + " == " + subjects); //now i make a to show records
-         }
-        }
+       for(String subjects : list_subject){
+           System.out.println(grades + " == " + subjects); //now i make a to show records
+       }
+     }
     }
     
     void isPassed(){
-        for(double passed : list_grade){
-            if(passed >= 90){
-                System.out.println(" Passed: A");
-           }else if(passed >= 80){
-                System.out.println(" Passed: B");
-           }else if(passed >= 75){
-                System.out.println(" Passed: C");
-           }else{
-                System.out.println(" Failed");
+        
+        for(int i = 0; i < list_grade.length; i++){
+        double ave = list_grade[i] + list_grade[i] / 4;
+            System.out.println(ave);
+            
+            if(ave > 75){
+                System.out.println("Passed");
+            }else{
+                System.out.println("Failed");
             }
         }
-        
-        System.out.println("");
-        
-     
+            
     }
+
 }
